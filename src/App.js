1a411aaa21fbe6592969b8817b70190984604bf2
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-24 14:14:03 +0200

    search fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Search from "./components/searchPage/Search";
 
--- src/components/searchPage/Search.jsx
+++ src/components/searchPage/Search.jsx
@@ -1,22 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { Modal, FormControl } from "react-bootstrap";
 
 const Search = () => {
   const [show, setShow] = useState(false);
+  const [data, setData] = useState(undefined);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=678b131125c510fd6d8c5158e03f8e33`
+        );
+        if (response.ok) {
+          const res = await response.json();
+          console.log("in fetch", res.name);
+          setData(res);
+        } else {
+          console.log("error in search fetch");
+        }
+      } catch (error) {
+        console.log("catcha", error);
+      }
+    };
+    fetchData();
+  }, [query]);
 
   return (
     <>
       <AiOutlinePlus onClick={() => setShow(true)} />
-      {console.log("Nodeenv", process.env.NODE_ENV)}
       {console.log("apikey", process.env.API_KEY)}
+      {console.log("data", data)}
       <Modal
         show={show}
         onHide={() => setShow(false)}
         dialogClassName="modal-60w"
         aria-labelledby="example-custom-modal-styling-title"
       >
-        <FormControl />
+        <FormControl value={query} onChange={(e) => setQuery(e.target.value)} />
       </Modal>
     </>
   );
