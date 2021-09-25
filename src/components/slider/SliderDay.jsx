a7223f1c847522783981cb14a96586d8eefc3ee8
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-25 13:40:57 +0200

    Create SliderDay.jsx

---
+++ src/components/slider/SliderDay.jsx
@@ -0,0 +1,65 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { WiHumidity } from "react-icons/wi";
+import { FaTemperatureHigh } from "react-icons/fa";
+import { format } from "date-fns";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 6,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 5,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 4,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 4,
+  },
+};
+
+const SliderDay = ({ daily }) => {
+  return (
+    <>
+      <Carousel responsive={responsive} className=" bg-warning">
+        {daily.map((time, i) => (
+          <>
+            <div className="d-flex flex-column">
+              <span className=" border-bottom">
+                {format(new Date(time.dt * 1000), "ccc")}
+              </span>
+              <div>
+                <img
+                  className="mt-n3"
+                  src={`http://openweathermap.org/img/wn/${time.weather[0].icon}@2x.png`}
+                  alt=""
+                />
+              </div>
+              <span className="mt-n4 ">
+                <WiHumidity />
+                {time.humidity}%
+              </span>
+              <div className="my-4 d-flex flex-column">
+                <span>hi</span>
+                <span>
+                  <FaTemperatureHigh style={{ fontSize: "30px" }} />
+                </span>
+                <span>hoho</span>
+              </div>
+            </div>
+          </>
+        ))}
+      </Carousel>
+      <div>hey</div>
+      <div>hey</div>
+      <div>hey</div>
+    </>
+  );
+};
+export default SliderDay;
