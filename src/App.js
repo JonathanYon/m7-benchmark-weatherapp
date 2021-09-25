9f37958619d861c2c0c8deee69cf506c6a9bd138
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-25 16:41:49 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 import Search from "./components/searchPage/Search";
 
 function App() {
   return (
-    <div className="App">
-      <Search />
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(https://img.freepik.com/free-vector/elegant-cloud-background-blue-sky-design_1017-25503.jpg?size=626&ext=jpg)`,
+      }}
+    >
+      <Container>
+        {/* <Row> */}
+        <Search />
+        {/* </Row> */}
+        <Row>
+          <Col sm={6}>hi</Col>
+          <Col sm={6}>hello</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
