48f03852273921e7515946327cca9c1a5fb8081f
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-26 14:18:03 +0200

    wether reducer

---
+++ src/redux/reducer/weather.js
@@ -0,0 +1,25 @@
+import { initialState } from "../store";
+import { GET_WEATHER, SET_ERROR, SET_LOADING } from "../action";
+
+const weatherReducer = (state = initialState.weather, action) => {
+  switch (action.type) {
+    case GET_WEATHER:
+      return {
+        ...state,
+        datail: [...state.datail, action.payload],
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+export default weatherReducer;
