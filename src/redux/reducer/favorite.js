8683ae4caaeb91e6ccae6ca5fc8bb7cc577cbf2c
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-26 14:43:30 +0200

    fav reducer

---
+++ src/redux/reducer/favorite.js
@@ -0,0 +1,13 @@
+import { initialState } from "../store";
+import { TOGGLE_FAV } from "../action";
+
+const favReducer = (state = initialState.favorite, action) => {
+  switch (action.type) {
+    case TOGGLE_FAV:
+      return {
+        ...state,
+        fav: state.cities.concat(action.payload),
+      };
+  }
+};
+export default favReducer;
